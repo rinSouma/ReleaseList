bcd646f3707c83a28eb071fd62be30d326e479e0
rin <souma_rin7th@yahoo.co.jp>
2019-07-08 17:41:27 +0900

    trelloに必要な分だけ作成

--- app/controllers/concerns/trello.rb
+++ app/controllers/concerns/trello.rb
@@ -4,61 +4,90 @@ require 'json'
 
 module Trello
     extend ActiveSupport::Concern
-    #ユーザIDの取得
-    def get_user_id(token)
-        res = run_api("https://trello.com/1/tokens/" + token + "?key=" + ENV['TRELLO_API_KEY'])
-        json_data = JSON.load(res.body)
-        return json_data["idMember"]
-    end
-    #ボード一覧の取得
-    def get_boards(token)
-        user_id = get_user_id(token)
-        res = run_api("https://trello.com/1/members/" + user_id +  "/boards?token=" + token + "&key=" + ENV['TRELLO_API_KEY'])
-        json_data = JSON.load(res.body)
-        board_list = []
-        json_data.each do | board_data |
-            if board_data["closed"].to_s == "false" then
-                board_list << {id:board_data["id"].to_s,name:board_data["name"].to_s}
-            end
+    class Trello_API
+        attr_accessor :name, :desc, :pos, :due, :due_complete, :id_list, :id_labels
+
+        def initialize(token, api_key)
+            @token = token
+            @api_key = api_key
         end
-        return board_list
-    end
 
-    #リスト一覧の取得
-    def get_lists(token, board_id)
-
-    end
-
-    #ラベル一覧の取得
-    def get_labels(token, board_id)
-
-    end
-
-    #カードの登録
-    def set_card(token, in_data)
-
-    end
-
-    private
-
-    def run_api(api_url, params=nil)
-        logger.debug(api_url)
-        uri = URI.parse(api_url)
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = true
-        #postパラメタの有無で処理を変える
-        if params.present? then
-            req = Net::HTTP::Post.new(uri.path)
-            req.set_form_data(params)
-            res = https.start {
-                https.request(req)
-            }
-            return res
-        else
-            res = https.start {
-                https.get(uri.request_uri)
-            }
-            return res
-        end
+	    #ユーザIDの取得
+	    def get_user_id
+	        res = run_api("https://trello.com/1/tokens/" + @token + "?key=" + @api_key)
+	        json_data = JSON.load(res.body)
+	        return json_data["idMember"]
+	    end
+	    #ボード一覧の取得
+	    def get_boards
+	        user_id = get_user_id
+	        res = run_api("https://trello.com/1/members/" + user_id +  "/boards?token=" + @token + "&key=" + @api_key)
+	        json_data = JSON.load(res.body)
+	        board_list = []
+	        json_data.each do | board_data |
+	            if board_data["closed"].to_s == "false" then
+	                board_list << {id:board_data["id"].to_s,name:board_data["name"].to_s}
+	            end
+	        end
+	        return board_list
+	    end
+	
+	    #リスト一覧の取得
+	    def get_lists(board_id)
+	        res = run_api("https://trello.com/1/boards/" + board_id.to_s +  "/lists?token=" + @token + "&key=" + @api_key)
+	        json_data = JSON.load(res.body)
+	        list_list = []
+	        json_data.each do | list_data |
+	            if list_data["closed"].to_s == "false" then
+	                list_list << {id:list_data["id"].to_s,name:list_data["name"].to_s,board:board_id}
+	            end
+	        end
+	        return list_list
+	    end
+	
+	    #ラベル一覧の取得
+	    def get_labels(board_id)
+	        res = run_api("https://trello.com/1/boards/" + board_id.to_s +  "/labels?token=" + @token + "&key=" + @api_key)
+	        json_data = JSON.load(res.body)
+	        label_list = []
+	        json_data.each do | label_data |
+	            if label_data["name"].to_s.present? then
+	                label_list << {id:label_data["id"].to_s,name:label_data["name"].to_s,board:board_id}
+	            end
+	        end
+	        return label_list
+	    end
+	
+	    #カードの登録
+        def set_card
+            params = {'name' => @name.to_s, 'desc' => @desc.to_s, 'pos' => @pos.to_s, \
+                        'due' => @due, 'dueComplete' => @due_complete, \
+                        'idList' => @id_list.to_s, 'idLabels' => @id_labels.to_s, \
+                        'token' => @token, 'key' =>  @api_key}
+            res = run_api("https://api.trello.com/1/cards", params)
+	    end
+	
+	    private
+	
+	    def run_api(api_url, params=nil)
+	        puts api_url
+	        uri = URI.parse(api_url)
+	        https = Net::HTTP.new(uri.host, uri.port)
+	        https.use_ssl = true
+	        #postパラメタの有無で処理を変える
+	        if params.present? then
+	            req = Net::HTTP::Post.new(uri.path)
+	            req.set_form_data(params)
+	            res = https.start {
+	                https.request(req)
+	            }
+	            return res
+	        else
+	            res = https.start {
+	                https.get(uri.request_uri)
+	            }
+	            return res
+	        end
+	    end
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,22 @@
 class HomeController < ApplicationController
     include Trello
     def index
-        @data = get_boards(session[:token])
+        test = Trello_API.new(session[:token], ENV['TRELLO_API_KEY'])
+        test.name = "test_name "
+        test.desc = "test_desc "
+        test.pos = "bottom"
+        test.due = Time.now
+        test.due_complete = false
+        test.id_list = "5d22c4a0039fd444064481de"
+        test.id_labels = "5d22c498af988c41f2ae98c1"
+        test.set_card
+
+        @board = test.get_boards
+        @list = []
+        @label = []
+        @board.each do | board_data |
+            @list << test.get_lists(board_data[:id])
+            @label << test.get_labels(board_data[:id])
+        end
     end
 end
